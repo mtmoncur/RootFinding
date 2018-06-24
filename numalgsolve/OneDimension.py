61303401626ece5f3bb43c6078e6b9ac0fc5b59d
Tyler Moncur <mtmoncur@gmail.com>
2018-06-24 16:54:37 -0600

    unnecessary changes, committing in order to change branch

--- numalgsolve/OneDimension.py
+++ numalgsolve/OneDimension.py
@@ -3,6 +3,7 @@ from scipy.linalg import eig, norm, eigvals
 from numpy import linalg as la
 from numalgsolve.polynomial import MultiCheb, MultiPower
 
+from matplotlib import pyplot as plt
 def solve(poly, method = 'mult', eigvals=True):
     """Finds the zeros of a 1-D polynomial.
 
@@ -64,6 +65,56 @@ def multPower(coeffs, eigvals=True):
         vals,vecs = eig(matrix.T)
         return vecs[1,:]/vecs[0,:]
 
+def logplot(vals):
+    z = np.sign(vals)*np.log(np.abs(vals)+1)
+    plt.imshow(z)
+    plt.colorbar()
+    plt.show()
+
+def multnPower(coeffs, mult_n, eigvals=True):
+    """Finds the zeros of a 1-D power polynomial using a multiplication matrix.
+
+    Parameters
+    ----------
+    coeffs : numpy array
+        The coefficients of the polynomial.
+    mult_n : int
+        The power of x for the multiplication matrix.
+    Returns
+    -------
+    zero : numpy array
+        An array of the zeros.
+    """
+    n = len(coeffs) - 1
+    matrix = np.eye(n, dtype=coeffs.dtype)
+
+    xn = -(coeffs[:-1]/coeffs[-1]).reshape((n,1))
+    for i in range(mult_n):
+        remainder = matrix[-1,-i:].copy()
+        matrix[-1] = 0
+        matrix = np.roll(matrix, 1, axis=0)
+        matrix[:,-i:] += remainder*xn
+
+    plt.imshow(np.abs(matrix))
+    plt.title(f"{mult_n}")
+    plt.colorbar()
+    plt.show()
+
+
+    if eigvals:
+        zeros = la.eigvals(np.rot90(matrix,2))**(1/mult_n)
+        return zeros
+    else:
+        vals,vecs = eig(np.rot90(matrix,2), left=True, right=False)
+        print(vecs[-5:,0])
+        return np.conjugate(vecs[-2,:]/vecs[-1,:])
+        # vals,vecs = eig(np.rot90(matrix,2), left=True, right=False)
+        # return np.conjugate(vecs[0,:]/vecs[1,:])
+
+        # idx = np.argmax(np.abs(vecs), axis=0)
+        # return np.conjugate(vecs[idx-1,:]/vecs[idx,:])
+        # vals,vecs = eig(matrix, left=True, right=False)
+        # return vecs[0,:]/vecs[1,:]
 
 def multPowerR(coeffs, eigvals=True):
     """Finds the zeros of a 1-D power polynomial using a multiplication matrix.
@@ -84,12 +135,12 @@ def multPowerR(coeffs, eigvals=True):
     bot[...] = 1
     matrix[:, -1] -= coeffs[:-1]/coeffs[-1]
     if eigvals:
-        zeros = la.eigvals(np.rot90(matrix,2).copy())
+        zeros = la.eigvals(np.rot90(matrix,2))
         return zeros
     else:
         from scipy.linalg import eig
-        vals,vecs= eig(np.rot90(matrix,2).copy(), left=True, right=False)
-        return np.conjugate(vecs[-2,:]/vecs[-1,:])
+        vals,vecs= eig(np.rot90(matrix,2), left=True, right=False)
+        return np.conjugate(vecs[0,:]/vecs[1,:])
 
 def divPower(coeffs, eigvals=True):
     """Finds the zeros of a 1-D power polynomial using a division matrix.
@@ -107,15 +158,142 @@ def divPower(coeffs, eigvals=True):
     n = len(coeffs) - 1
     matrix = np.zeros((n, n), dtype=coeffs.dtype)
     bot = matrix.reshape(-1)[1::n+1]
-    bot[...] = 1#*coeffs[0]
+    bot[...] = 1
     matrix[:, 0] -= coeffs[1:]/coeffs[0]
-
+    # print(matrix)
     if eigvals:
         zeros = 1/la.eigvals(matrix)
-        return zeros#*coeffs[0]
+        return zeros
     else:
         from scipy.linalg import eig
-        vals,vecs,_ = eig(matrix, left=True)
+        vals,vecs = eig(matrix, left=True, right=False)
+        return np.conjugate(vecs[1,:]/vecs[0,:])
+
+def div2Power(coeffs, eigvals=True):
+    """Finds the zeros of a 1-D power polynomial using a twice division matrix.
+
+    Parameters
+    ----------
+    coeffs : numpy array
+        The coefficients of the polynomial.
+
+    Returns
+    -------
+    zero : numpy array
+        An array of the zeros.
+    """
+    n = len(coeffs) - 1
+    matrix = np.zeros((n, n), dtype=coeffs.dtype)
+    bot = matrix.reshape(-1)[2:n*(n-2):n+1]
+    bot[...] = 1*coeffs[0]
+    # print(matrix)
+    # print(coeffs)
+    matrix[:, 1] -= coeffs[1:]#/coeffs[0]
+    matrix[:, 0] -= coeffs[1]*matrix[:, 1]/coeffs[0]
+    matrix[:-1, 0] -= coeffs[2:]#/coeffs[0]
+    # print(matrix)
+
+    from matplotlib import pyplot as plt
+    # plt.imshow(matrix.real)
+    # plt.colorbar()
+    # plt.show()
+    if eigvals:
+        zeros = 1/la.eigvals(matrix)
+        return zeros/coeffs[0]
+    else:
+        from scipy.linalg import eig
+        vals,vecs = eig(matrix, left=True, right=False)
+        return np.conjugate(vecs[1,:]/vecs[0,:])
+
+def divnPower(coeffs, div_n, eigvals=True):
+    """Finds the zeros of a 1-D power polynomial using an nth division matrix.
+
+    Parameters
+    ----------
+    coeffs : numpy array
+        The coefficients of the polynomial.
+    div_n : int
+        Use x^(-n) for functions
+
+    Returns
+    -------
+    zero : numpy array
+        An array of the zeros.
+    """
+    assert div_n > 0
+    n = len(coeffs) - 1
+    matrix = np.eye(n, dtype=coeffs.dtype)
+    #bot = matrix.reshape(-1)[2:n*(n-2):n+1]
+    #bot[...] = 1*coeffs[0]
+    # print(matrix)
+    # print(coeffs)
+    xinv = (-coeffs[1:]/coeffs[0]).reshape((n,1))
+    # print('xinv',xinv)
+    remainder = np.zeros(n)
+    for i in range(div_n):
+        remainder = matrix[0, :i+1].copy()
+        matrix[0] = 0
+        matrix = np.roll(matrix, -1, axis=0)
+        matrix[:,:i+1] += remainder*xinv
+        # print("i\n", matrix)
+    # matrix[:, 1] -= coeffs[1:]#/coeffs[0]
+    # matrix[:, 0] -= coeffs[1]*matrix[:, 1]/coeffs[0]
+    # matrix[:-1, 0] -= coeffs[2:]#/coeffs[0]
+    # print(matrix)
+
+    from matplotlib import pyplot as plt
+    # plt.imshow(matrix.real)
+    # plt.colorbar()
+    # plt.show()
+    if eigvals:
+        zeros = la.eigvals(matrix)**(-1/div_n)
+        return zeros
+    else:
+        from scipy.linalg import eig
+        vals,vecs = eig(matrix, left=True, right=False)
+        return np.conjugate(vecs[1,:]/vecs[0,:])
+
+def plusPower(coeffs, one_n, eigvals=True):
+    """Finds the zeros of a 1-D power polynomial using an nth division matrix.
+
+    Parameters
+    ----------
+    coeffs : numpy array
+        The coefficients of the polynomial.
+    div_n : int
+        Use x^(-n) for functions
+
+    Returns
+    -------
+    zero : numpy array
+        An array of the zeros.
+    """
+    n = len(coeffs) - 1
+    dMatrix = np.zeros((n, n), dtype=coeffs.dtype)
+    bot = dMatrix.reshape(-1)[1::n+1]
+    bot[...] = 1
+    dMatrix[:, 0] -= coeffs[1:]/coeffs[0]
+
+    matrix = np.zeros((n, n), dtype=coeffs.dtype)
+    bot = matrix.reshape(-1)[n::n+1]
+    bot[...] = 1#*coeffs[-1]
+    matrix[:, -1] -= coeffs[:-1]/coeffs[-1]
+
+    matrix += dMatrix
+
+    from matplotlib import pyplot as plt
+    logplot(np.abs(matrix))
+
+    # zeros = x + 1/x
+    # x^2 - zeros*z + 1 = 0
+    if eigvals:
+        zeros = la.eigvals(matrix)
+        zeros = (zeros - np.sqrt(zeros**2 - 4))/(2)
+        zeros = np.append(zeros, (zeros + np.sqrt(zeros**2 - 4))/(2))
+        return zeros
+    else:
+        from scipy.linalg import eig
+        vals,vecs = eig(np.rot90(matrix,2), left=True, right=False)
         return np.conjugate(vecs[1,:]/vecs[0,:])
 
 def multCheb(coeffs, eigvals=True):
@@ -144,8 +322,40 @@ def multCheb(coeffs, eigvals=True):
         zeros = la.eigvals(mMatrix)
         return zeros
     else:
-        vals,vecs = eig(matrix.T)
+        vals,vecs = eig(mMatrix.T)
         return vecs[1,:]/vecs[0,:]
+
+def multChebR(coeffs, eigvals=True):
+    """Finds the zeros of a 1-D chebyshev polynomial using a multiplication matrix.
+
+    Parameters
+    ----------
+    coeffs : numpy array
+        The coefficients of the polynomial.
+
+    Returns
+    -------
+    zero : numpy array
+        An array of the zeros.
+    """
+    n = len(coeffs) - 1
+    mMatrix = np.zeros((n,n), dtype=coeffs.dtype)
+    mMatrix[1][0] = 1
+    bot = mMatrix.reshape(-1)[1::n+1]
+    bot[...] = 1/2
+    bot = mMatrix.reshape(-1)[2*n+1::n+1]
+    bot[...] = 1/2
+    #print(coeffs[-1])
+    mMatrix[:,-1] -= .5*coeffs[:-1]/coeffs[-1]
+    if eigvals:
+        zeros = la.eigvals(np.rot90(mMatrix,2))
+        return zeros
+    else:
+        vals,vecs = eig(np.rot90(mMatrix,2))
+        plt.imshow(vecs.real)
+        plt.colorbar()
+        plt.show()
+        return vecs[0,:]/vecs[1,:]
 
 
 def getXinv(coeff):
@@ -154,11 +364,11 @@ def getXinv(coeff):
     xinv = np.zeros(n, dtype=coeff.dtype)
     for i in range(1,n)[::-1]:
         val = -curr[i+1]
-        curr[i+1] += val
+        # curr[i+1] += val
         curr[i-1] += val
         xinv[i]+=2*val
     temp = -curr[1]
-    curr[1]+=temp
+    # curr[1]+=temp
     xinv[0]+=temp
     #xinv/=curr[0]
     return xinv,curr[0]
@@ -269,7 +479,7 @@ def div2Cheb(coeffs, eigvals=True):
         dMatrix[:,col]+= x2inv*sign
         sign*=-1
 
-    if eigvals;
+    if eigvals:
         zerosD = 1/la.eigvals(dMatrix)
 
     else:
@@ -281,6 +491,7 @@ def div2Cheb(coeffs, eigvals=True):
 
     if abs(divisor) > 1:
         pass
-        # return zerosD
+        return zerosD**(1/2)
     else:
         zerosD *= divisor
+        return zerosD**(1/2)
